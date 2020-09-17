dff7606e8e11acd2186fa9b962c3b8ff802b0afb
Trudzer <trudzeredits@gmail.com>
2020-09-17 10:58:56 +0000

    grp project

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,1 +1,28 @@
+def walk(direction, position):
 
+  if direction == "e":
+    position += 1
+  
+  if direction == "n":
+    position += 10
+
+  if direction == "w":
+    position -= 1
+
+  if direction == "s":
+    position -= 10
+  else:
+    print("invalid input")
+  
+  return position
+pos = 11
+while pos != 33:
+  inp = input("choose direction: ").lower()
+  direct_inp = walk(inp, pos)
+  if direct_inp > 0 and direct_inp < 34:
+    pos = direct_inp
+  else:
+    print("invalid direction")
+
+  direct_inp = 0
+print("congratulations you win")
